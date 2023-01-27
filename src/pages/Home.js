e3dd37d5a9414ba0f9ce12ccabdacecb003fe8d4
johnlombardi389 <lombardij38@gmail.com>
2023-01-27 16:23:34 -0500

    Created supabase client file and set up fetch data in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,40 @@
+import { useState, useEffect } from "react";
+import supabase from "../config/supabaseClient";
+
 const Home = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from("smoothies").select();
+
+      if (error) {
+        setFetchError("Could not fetch the smoothies");
+        setSmoothies(null);
+        console.log(error);
+      }
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchSmoothies();
+  }, []);
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          {smoothies.map((smoothie) => (
+            <p>{smoothie.title}</p>
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
