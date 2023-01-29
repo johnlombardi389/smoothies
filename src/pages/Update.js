e0f73de26b3cb9aa833efb4c4ec2440e56fb9ce0
johnlombardi389 <lombardij38@gmail.com>
2023-01-28 19:10:40 -0500

    Set up data fetch for individual smoothie when clicking the edit icon button

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,7 +1,35 @@
-import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import supabase from "../config/supabaseClient";
 
 const Update = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [rating, setRating] = useState("");
+
+  useEffect(() => {
+    const fetchSmoothie = async () => {
+      const { data, error } = await supabase
+        .from("smoothies") // which table to fetch data from
+        .select() // grab all smoothies
+        .eq("id", id) // select smoothies where column id matches id (returns array)
+        .single(); // return single item to get single smoothie from table
+
+      if (error) {
+        navigate("/", { replace: true });
+      }
+      if (data) {
+        setTitle(data.title);
+        setMethod(data.method);
+        setRating(data.rating);
+      }
+    };
+
+    fetchSmoothie();
+  }, [id, navigate]);
 
   return (
     <div className="page update">
